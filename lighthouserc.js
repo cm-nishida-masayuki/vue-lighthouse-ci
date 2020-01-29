325e256a19ee05b45266380260be6cb1ecb7bbdc
nishida.masayuki <nishida.masayuki@classmethod.jp>
2020-01-29 21:46:40 +0900

    first commit

---
+++ lighthouserc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 1,
+      staticDistDir: "./dist",
+      url: [
+        "/"
+      ]
+    },
+    "assert": {
+      "preset": "lighthouse:no-pwa",
+      "assertions": {
+        "offscreen-images": "off",
+        "uses-webp-images": "off",
+        "uses-http2": "off",
+      }
+    },
+    "upload": {
+      "target": "temporary-public-storage"
+    }
+  }
+}
